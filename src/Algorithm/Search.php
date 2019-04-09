4a2e14c2f257995a4c70e4b5825620660da6bd70
Dmitry Tymko <dimatymko@gmail.com>
2019-04-09 16:42:57 +0300

    feature: Algorithm -Search(breadth-first search)

--- src/Algorithm/Search.php
+++ src/Algorithm/Search.php
@@ -2,25 +2,28 @@
 
 namespace Algorithm;
 
+use SplDoublyLinkedList;
+use SplQueue;
+
 class Search
 {
     /**
      * Binary search
      *
      * @param array $arraySort the array is sorted
-     * @param int   $item conceived number
-     * @return int/null number of comparisons
+     * @param int $item conceived number
+     * @return int number of comparisons
      */
-    public static function binarySearch(array $arraySort, int $item)
+    public static function binarySearch(array $arraySort, int $item): int
     {
         $number = 0;
         $low    = 0;
         $high   = count($arraySort) - 1;
-        
+
         while ($low <= $high) {
             $middle = (int)round(($low + $high) / 2, PHP_ROUND_HALF_DOWN);
             $number++;
-            
+
             switch ($arraySort[$middle]) {
                 case $item:
                     return $number;
@@ -32,7 +35,54 @@ class Search
                     break;
             }
         }
-        
-        return null;
+
+        return 0;
+    }
+
+    /**
+     * Breadth-first search
+     *
+     * @param array $graph there is a graph
+     * @param mixed $name first element
+     * @param mixed $target what should find
+     * @return int number of plucked element
+     */
+    public static function breadthFirstSearch(array $graph, $name, $target): int
+    {
+        $queue = new SplQueue;
+        $queue->setIteratorMode(SplDoublyLinkedList::IT_MODE_DELETE);
+        self::enqueue($queue, $graph[$name]);
+
+        $searched[$name] = true;
+        $count           = 0;
+
+        while (!$queue->isEmpty()) {
+            $element = $queue->dequeue();
+
+            if (isset($searched[$element]))
+                continue;
+
+
+            $count++;
+
+            if ($element === $target) {
+                return $count;
+                
+            } else {
+                self::enqueue($queue, $graph[$element]);
+                $searched[$element] = true;
+            }
+        }
+
+        return 0;
+    }
+
+    private static function enqueue(SplQueue $queue, array $graph): void
+    {
+        foreach ($graph as $item) {
+            $queue->enqueue($item);
+        }
+
+        return;
     }
 }
